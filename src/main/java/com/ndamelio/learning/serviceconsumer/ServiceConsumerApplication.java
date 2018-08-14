9f3efcd79bf117d7831429c0b93e47e3db0842ea
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-14 12:35:57 -0300

    first commit

---
+++ src/main/java/com/ndamelio/learning/serviceconsumer/ServiceConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.learning.serviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConsumerApplication.class, args);
+    }
+}
